180cff6ed35bfbc22b1029cbb0562d102e16c7a9
joewong00 <63445582+joewong00@users.noreply.github.com>
2020-04-19 15:49:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -35,56 +35,40 @@ if(isset($_GET["table"])){
 		echo "</form>";
 		echo "<br>";
 			
-	
 	}
 	else if($table == "country"){
-		echo "<table border='1' cellpadding='2'>";
-		echo "<tr>";
-			echo"<th>Country_abb</th>";
-			echo"<th>Country_Name</th>";
-			echo"<th>Continent</th>";
-			echo"<th>Region</th>";
-			echo"<th>Area</th>";
-			echo"<th>Year_of_Ind</th>";
-			echo"<th>Population</th>";
-			echo"<th>Life_Expectancy</th>";
-			echo"<th>Gnp</th>";
-			echo"<th>Gnp_Old</th>";
-			echo"<th>Local_Name</th>";
-			echo"<th>Gov_Form</th>";
-			echo"<th>Head_of_State</th>";
-			echo"<th>Capital</th>";
-			echo"<th>Country_Code</th>";
-		echo"</tr>";
-
-		$sql = "SELECT * FROM country;";
-		$result = mysqli_query($conn, $sql);
-		
-
-		
-			while($row = mysqli_fetch_assoc($result)){
-				echo "<tr><td>".$row["Country_abb"]."</td><td>".$row["Country_Name"]."</td><td>".$row["Continent"]."</td><td>".$row["Region"]."</td><td>".$row["Area"]."</td><td>".$row["Year_of_Ind"]."</td><td>".$row["Population"]."</td><td>".$row["Life_Expectancy"]."</td><td>".$row["Gnp"]."</td><td>".$row["Gnp_Old"]."</td><td>".$row["Local_Name"]."</td><td>".$row["Gov_Form"]."</td><td>".$row["Head_of_State"]."</td><td>".$row["Capital"]."</td><td>".$row["Country_Code"]."</td></tr>";
-			}
-			echo "</table>";
+		echo "<form action = 'table2.php' method = 'get'>";
+		echo "Country_abb<input type = 'checkbox' name = 'col2[]' value = 'Country_abb'>";
+		echo " Country_Name<input type = 'checkbox' name = 'col2[]' value = 'Country_Name'>";
+		echo " Continent<input type = 'checkbox' name = 'col2[]' value = 'Continent'>";
+		echo " Region<input type = 'checkbox' name = 'col2[]' value = 'Region'>";
+		echo " Area<input type = 'checkbox' name = 'col2[]' value = 'Area'>";
+		echo " Year_of_Ind<input type = 'checkbox' name = 'col2[]' value = 'Year_of_Ind'>";
+		echo " Population<input type = 'checkbox' name = 'col2[]' value = 'Population'>";
+		echo " Life_Expectancy<input type = 'checkbox' name = 'col2[]' value = 'Life_Expectancy'>";
+		echo " Gnp<input type = 'checkbox' name = 'col2[]' value = 'Gnp'>";
+		echo " Gnp_Old<input type = 'checkbox' name = 'col2[]' value = 'Gnp_Old'>";
+		echo " Local_Name<input type = 'checkbox' name = 'col2[]' value = 'Local_Name'>";
+		echo " Gov_Form<input type = 'checkbox' name = 'col2[]' value = 'Gov_Form'>";
+		echo " Head_of_State<input type = 'checkbox' name = 'col2[]' value = 'Head_of_State'>";
+		echo " Capital<input type = 'checkbox' name = 'col2[]' value = 'Capital'>";
+		echo " Country_Code<input type = 'checkbox' name = 'col2[]' value = 'Country_Code'>";
+		echo "<br>";
+		echo "<input type = 'submit' name = 'submit' value = 'SELECT'>";
+		echo "</form>";
+		echo "<br>";
 		
 	}
 	else if($table == "language"){
-		echo "<table border='1' cellpadding='2'>";
-		echo "<tr>";
-			echo"<th>Country_abb</th>";
-			echo"<th>Language</th>";
-			echo"<th>Official</th>";
-			echo"<th>Percentage</th>";
-		echo "</tr>";
-
-		$sql = "SELECT * FROM lang;";
-		$result = mysqli_query($conn, $sql);
-		
-		
-			while($row = mysqli_fetch_assoc($result)){
-				echo "<tr><td>".$row["Country_abb"]."</td><td>".$row["Language"]."</td><td>".$row["Official"]."</td><td>".$row["Percentage"]."</td></tr>";
-			}
-			echo "</table>";
+		echo "<form action = 'table3.php' method = 'get'>";
+		echo "Country_abb<input type = 'checkbox' name = 'col3[]' value = 'Country_abb'>";
+		echo " Language<input type = 'checkbox' name = 'col3[]' value = 'Language'>";
+		echo " Official<input type = 'checkbox' name = 'col3[]' value = 'Official'>";
+		echo " Percentage<input type = 'checkbox' name = 'col3[]' value = 'Percentage'>";
+		echo "<br>";
+		echo "<input type = 'submit' name = 'submit' value = 'SELECT'>";
+		echo "</form>";
+		echo "<br>";
 		
 	}
 	else
