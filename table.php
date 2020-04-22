a8014356e8823f83b3fd60d94912749d425bc733
joewong00 <63445582+joewong00@users.noreply.github.com>
2020-04-22 21:50:44 +0800

    Update table.php

--- table.php
+++ table.php
@@ -1,7 +1,25 @@
 <?php
     include_once 'demos/demo1.php';
+    include 'cwindex.php';
     
-    echo "<br>";
+    if ($_GET['submit']=='INSERT'){ //if we pressed insert
+        echo "Enter information:";
+        echo "<br>";
+        echo "<i>For inserting a text, please enclosed text with quotation(' ')</i>";
+        echo "<br>";
+        echo "<br>";
+        echo "<form action = 'insert1.php' method = 'get'>";
+        if(!empty($_GET["col"])){
+            foreach($_GET["col"] as $col){
+                echo "$col<input type = 'checkbox', value = '$col', name = 'col[]' checked> : <input type = 'text' name = 'inscol[]'>";
+                echo "<br>";
+                echo "<br>";
+            }
+            echo "<input type = 'submit' name = 'insert' value = 'submit'>";
+        }
+        
+        echo "</form>";
+        echo "<br>";
     echo "<table border='1' cellpadding='2'>";
     echo "<tr>";
     if(isset($_GET['submit'])){
@@ -9,6 +27,7 @@
             foreach($_GET["col"] as $col){
                 echo"<th>$col</th>";
             }
+            echo"<th>ACTIONS</th>";
         echo"</tr>";
 
 		$sql = "SELECT * FROM city;";
@@ -19,6 +38,7 @@
                 foreach($_GET["col"] as $col){
                     echo "<td>".$row["$col"]."</td>";
                 }
+                echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
                 echo "</tr>";
             }
         }
@@ -26,5 +46,37 @@
             echo "Please select at least one column";
         }
     }
-		echo "</table>";
+        echo "</table>";
+    }
+
+    else { //if we pressed select
+    echo "<br>";
+    echo "<table border='1' cellpadding='2'>";
+    echo "<tr>";
+    if(isset($_GET['submit'])){
+        if(!empty($_GET["col"])){
+            foreach($_GET["col"] as $col){
+                echo"<th>$col</th>";
+            }
+            echo"<th>ACTIONS</th>";
+        echo"</tr>";
+
+		$sql = "SELECT * FROM city;";
+        $result = mysqli_query($conn, $sql);
+        
+			while($row = mysqli_fetch_assoc($result)){
+                echo "<tr>";
+                foreach($_GET["col"] as $col){
+                    echo "<td>".$row["$col"]."</td>";
+                }
+                echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
+                echo "</tr>";
+            }
+        }
+        else{
+            echo "Please select at least one column";
+        }
+    }
+        echo "</table>";
+}
 ?>
