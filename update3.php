18a9255844f36b02619bced3854f118f7b85a492
joewong00 <63445582+joewong00@users.noreply.github.com>
2020-04-27 21:35:44 +0800

    Update update3.php

--- update3.php
+++ update3.php
@@ -15,25 +15,25 @@
               }
             
             if(mysqli_query($conn,$sql)){
-                echo "Values updated";
+                echo "<p> <font color=white>Values updated";
             }
             else
-                echo "Values not updated";
+                echo "<p> <font color=white>Values not updated";
 
             echo "<br>";
         }
     }
 
     echo "<br>";
-    echo "<table border='1' cellpadding='2'>";
+    echo "<table border='1' cellpadding='2' bordercolor='#fff200' width = '100%'>";
     echo "<tr>";
     array_unshift($column, 'Country_abb', 'Language'); 
     if(isset($_GET['update'])){
         if(!empty($_GET["updcol3"])){
             foreach($column as $col3){
-                echo"<th>$col3</th>";
+                echo"<th><p> <font color=white>$col3</th>";
             }
-            echo"<th>ACTIONS</th>";
+            echo"<th><p> <font color=white>ACTIONS</th>";
         echo"</tr>";
 
 		$sql = "SELECT * FROM lang;";
@@ -43,20 +43,20 @@
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($column as $col3){
-                    echo "<td>".$row["$col3"]."</td>";
+                    echo "<td><p> <font color=white>".$row["$col3"]."</td>";
                 }
-                echo '<td><form action="delete3.php" method="get">';
+                echo '<td align="center"><form action="delete3.php" method="get">';
                     foreach($column as $col3){
                         echo "<input type='hidden' value='$col3' name='col3[]'>";
                     }
                 echo'<input type="hidden" name="primKey[]" value='.$row["Country_abb"].'> <input type="hidden" name="primKey[]" value='.$row["Language"].'> 
-                <input type="submit" name="delete" value="DELETE">';
-                echo' <input type="submit" name="update" value="UPDATE"></form></td>'; 
+                <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete">';
+                echo' <input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>'; 
                 echo "</tr>";
             }
         }
         else{
-            echo "No value updated";
+            echo "<p> <font color=white>No value updated";
         }
     }
         echo "</table>";
