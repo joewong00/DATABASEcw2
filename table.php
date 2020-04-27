3e92ecb03cfb737bba5ae78c70393dc631e9c3cd
joewong00 <63445582+joewong00@users.noreply.github.com>
2020-04-27 21:27:09 +0800

    Update table.php

--- table.php
+++ table.php
@@ -4,8 +4,7 @@
 
     if ($_GET['submit']=='INSERT'){ //if we pressed insert
 
-        echo "Enter information:";
-        echo "<br>";
+        echo "<p> <font color=white>Enter information:";
         //echo "<i>For inserting a text, please enclose text with quotation(' ')</i>";
         echo "<br>";
         echo "<br>";
@@ -15,23 +14,23 @@
 
         if(!empty($_GET["col"])){
             foreach($_GET["col"] as $col){
-                echo "$col<input type = 'hidden', value = '$col', name = 'col[]'> : <input type = 'text' name = 'inscol[]'>";
+                echo "$col<input type = 'hidden', value = '$col', name = 'col[]'> : <input type = 'text' name = 'inscol[]' id='datacol' class='data'>";
                 echo "<br>";
                 echo "<br>";
             }
-            echo "<input type = 'submit' name = 'insert' value = 'submit'>";
+            echo "<input type = 'submit' name = 'insert' value = 'submit' id='submit1_btn' class='submit1'>";
         }
 
         echo "</form>";
         echo "<br>";
-    echo "<table border='1' cellpadding='2'>";
+    echo "<table border='1' cellpadding='2' bordercolor='#c23616' width = '100%'>";
     echo "<tr>";
     if(isset($_GET['submit'])){
         if(!empty($_GET["col"])){
             foreach($_GET["col"] as $col){
-                echo"<th>$col</th>";
+                echo"<th><p> <font color=white>$col</th>";
             }
-            echo"<th>ACTIONS</th>";
+            echo"<th><p> <font color=white>ACTIONS</th>";
         echo"</tr>";
 
 		$sql = "SELECT * FROM city;";
@@ -40,15 +39,19 @@
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($_GET["col"] as $col){
-                    echo "<td>".$row["$col"]."</td>";
+                    echo "<td><p> <font color=white>".$row["$col"]."</td>";
                 }
-                //echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
-                echo '<td><form action="delete1.php" method="get"> <input type="hidden" name="primKey" value='.$row["ID"].'> <input type="submit" name="delete" value="DELETE"> <input type="submit" name="update" value="UPDATE"></form></td>';
+                echo '<td align="center" ><form action="delete1.php" method="get">';
+                    foreach($_GET["col"] as $col){
+                        echo "<input type='hidden' value='$col' name='col[]'>";
+                    }
+                echo'<input type="hidden" name="primKey" value='.$row["ID"].'> <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete">';
+                echo'<input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>'; 
                 echo "</tr>";
             }
         }
         else{
-            echo "Please select at least one column";
+            echo "<p> <font color=white>Please select at least one column";
         }
     }
         echo "</table>";
@@ -57,14 +60,14 @@
     else { //if we pressed select
     echo "<b><ul><u>city</b></ul></u>";
     echo "<br>";
-    echo "<table border='1' cellpadding='2'>";
+    echo "<table border='1' cellpadding='2' bordercolor='#c23616' width = '100%'>";
     echo "<tr>";
     if(isset($_GET['submit'])){
         if(!empty($_GET["col"])){
             foreach($_GET["col"] as $col){
-                echo"<th>$col</th>";
+                echo"<th><p> <font color=white>$col</th>";
             }
-            echo"<th>ACTIONS</th>";
+            echo"<th><p> <font color=white>ACTIONS</th>";
         echo"</tr>";
 
 		$sql = "SELECT * FROM city;";
@@ -73,19 +76,19 @@
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($_GET["col"] as $col){
-                    echo "<td>".$row["$col"]."</td>";
+                    echo "<td><p> <font color=white>".$row["$col"]."</td>";
                 }
-                echo '<td><form action="delete1.php" method="get">';
+                echo '<td align="center" ><form action="delete1.php" method="get">';
                     foreach($_GET["col"] as $col){
                         echo "<input type='hidden' value='$col' name='col[]'>";
                     }
-                echo'<input type="hidden" name="primKey" value='.$row["ID"].'> <input type="submit" name="delete" value="DELETE">';
-                echo'<input type="submit" name="update" value="UPDATE"></form></td>'; 
+                echo'<input type="hidden" name="primKey" value='.$row["ID"].'> <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete">';
+                echo'<input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>'; 
                 echo "</tr>";
             }
         }
         else{
-            echo "Please select at least one column";
+            echo "<p> <font color=white>Please select at least one column";
         }
     }
         echo "</table>";
