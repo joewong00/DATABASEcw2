9e679676b2138b81c69998411f3dc41b72fad0bc
joewong00 <63445582+joewong00@users.noreply.github.com>
2020-04-27 15:45:26 +0800

    Update table2.php

--- table2.php
+++ table2.php
@@ -5,7 +5,7 @@
     if ($_GET['submit']=='INSERT'){ //if we pressed insert
         echo "Enter information:";
         echo "<br>";
-        echo "<i>For inserting a text, please enclosed text with quotation(' ')</i>";
+        //echo "<i>For inserting a text, please enclosed text with quotation(' ')</i>";
         echo "<br>";
         echo "<br>";
         echo "<form action = 'insert2.php' method = 'get'>";
@@ -17,7 +17,7 @@
             }
             echo "<input type = 'submit' name = 'insert' value = 'submit'>";
         }
-        
+
         echo "</form>";
         echo "<br>";
     echo "<table border='1' cellpadding='2'>";
@@ -32,13 +32,14 @@
 
 		$sql = "SELECT * FROM country;";
         $result = mysqli_query($conn, $sql);
-        
+
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($_GET["col2"] as $col2){
                     echo "<td>".$row["$col2"]."</td>";
                 }
-                echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
+                //echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
+                echo '<td><form action="delete2.php" method="get"> <input type="hidden" name="primKey" value='.$row["Country_abb"].'> <input type="submit" name="delete" value="DELETE"> <input type="submit" name="update" value="UPDATE"></form></td>';
                 echo "</tr>";
             }
         }
@@ -50,6 +51,7 @@
     }
 
     else{ //if we pressed select
+    echo "<b><ul><u>country</b></ul></u>";
     echo "<br>";
     echo "<table border='1' cellpadding='2'>";
     echo "<tr>";
@@ -63,13 +65,18 @@
 
 		$sql = "SELECT * FROM country;";
         $result = mysqli_query($conn, $sql);
-        
+
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($_GET["col2"] as $col2){
                     echo "<td>".$row["$col2"]."</td>";
                 }
-                echo "<td><input type = 'submit' value = 'UPDATE'> <input type = 'submit' value = 'DELETE'></td>";
+                echo '<td><form action="delete2.php" method="get">';
+                    foreach($_GET["col2"] as $col2){
+                        echo "<input type='hidden' value='$col2' name='col2[]'>";
+                    }
+                echo'<input type="hidden" name="primKey" value='.$row["Country_abb"].'> <input type="submit" name="delete" value="DELETE">';
+                echo'<input type="submit" name="update" value="UPDATE"></form></td>'; 
                 echo "</tr>";
             }
         }
@@ -79,4 +86,5 @@
     }
         echo "</table>";
 }
+
 ?>
