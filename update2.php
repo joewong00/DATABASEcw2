1d2ac89a0f9d32828c19b2352e129361712b2dbd
joewong00 <63445582+joewong00@users.noreply.github.com>
2020-04-27 21:35:21 +0800

    Update update2.php

--- update2.php
+++ update2.php
@@ -15,25 +15,25 @@
               }
             
             if(mysqli_query($conn,$sql)){
-                echo "Values updated";
+                echo "<p> <font color=white>Values updated";
             }
             else
-                echo "Values not updated";
+                echo "<p> <font color=white>Values not updated";
 
             echo "<br>";
         }
     }
 
     echo "<br>";
-    echo "<table border='1' cellpadding='2'>";
+    echo "<table border='1' cellpadding='2' bordercolor='#f0932b' width = '100%'>";
     echo "<tr>";
     array_unshift($column, 'Country_abb'); 
     if(isset($_GET['update'])){
         if(!empty($_GET["updcol2"])){
             foreach($column as $col2){
-                echo"<th>$col2</th>";
+                echo"<th><p> <font color=white>$col2</th>";
             }
-            echo"<th>ACTIONS</th>";
+            echo"<th><p> <font color=white>ACTIONS</th>";
         echo"</tr>";
 
 		$sql = "SELECT * FROM country;";
@@ -43,19 +43,19 @@
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($column as $col2){
-                    echo "<td>".$row["$col2"]."</td>";
+                    echo "<td><p> <font color=white>".$row["$col2"]."</td>";
                 }
-                echo '<td><form action="delete2.php" method="get">';
+                echo '<td align="center"><form action="delete2.php" method="get">';
                     foreach($column as $col2){
                         echo "<input type='hidden' value='$col2' name='col2[]'>";
                     }
-                echo'<input type="hidden" name="primKey" value='.$row["Country_abb"].'> <input type="submit" name="delete" value="DELETE">';
-                echo'<input type="submit" name="update" value="UPDATE"></form></td>'; 
+                echo'<input type="hidden" name="primKey" value='.$row["Country_abb"].'> <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete">';
+                echo'<input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>'; 
                 echo "</tr>";
             }
         }
         else{
-            echo "No value updated";
+            echo "<p> <font color=white>No value updated";
         }
     }
         echo "</table>";
