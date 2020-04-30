2bea6607a1aa97aa232335135e5650783b848747
joewong00 <63445582+joewong00@users.noreply.github.com>
2020-04-30 12:51:35 +0800

    Update update3.php

--- update3.php
+++ update3.php
@@ -13,21 +13,27 @@
                 $sql = "UPDATE lang SET $updsql WHERE Country_abb = '$key[0]' AND Language = '$key[1]'";
                 mysqli_query($conn,$sql);
               }
-            
+
             if(mysqli_query($conn,$sql)){
-                echo "<p> <font color=white>Values updated";
+                echo "<p> <font color=white>Values updated for $key[1] for $key[0]";
             }
-            else
+            else{
                 echo "<p> <font color=white>Values not updated";
 
+                echo "<br><br>Failed to update '$upd[$colkey]' into $column[$colkey]";
+                // print code
+                echo "<br><br>$sql<br>";
+                echo "Error: " .mysqli_error($conn);
+            }
             echo "<br>";
         }
     }
 
+
     echo "<br>";
     echo "<table border='1' cellpadding='2' bordercolor='#fff200' width = '100%'>";
     echo "<tr>";
-    array_unshift($column, 'Country_abb', 'Language'); 
+    array_unshift($column, 'Country_abb', 'Language');
     if(isset($_GET['update'])){
         if(!empty($_GET["updcol3"])){
             foreach($column as $col3){
@@ -38,8 +44,8 @@
 
 		$sql = "SELECT * FROM lang;";
         $result = mysqli_query($conn, $sql);
-        
-        
+
+
 			while($row = mysqli_fetch_assoc($result)){
                 echo "<tr>";
                 foreach($column as $col3){
@@ -49,9 +55,9 @@
                     foreach($column as $col3){
                         echo "<input type='hidden' value='$col3' name='col3[]'>";
                     }
-                echo'<input type="hidden" name="primKey[]" value='.$row["Country_abb"].'> <input type="hidden" name="primKey[]" value='.$row["Language"].'> 
+                echo'<input type="hidden" name="primKey[]" value='.$row["Country_abb"].'> <input type="hidden" name="primKey[]" value='.$row["Language"].'>
                 <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete">';
-                echo' <input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>'; 
+                echo' <input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>';
                 echo "</tr>";
             }
         }
