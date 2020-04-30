2b3e6a80d07adab6552193291fddea8f3427f24c
joewong00 <63445582+joewong00@users.noreply.github.com>
2020-04-30 12:44:04 +0800

    Update delete2.php

--- delete2.php
+++ delete2.php
@@ -2,25 +2,32 @@
     include_once 'demos/demo1.php';
     include 'cwindex.php';
 
-        $key = $_GET["primKey"];
-        $inssql = "'" . $key . "'";
-        //echo "$key";
+    $key = $_GET["primKey"];
+    $name = $_GET["name"];
+    $delsql = "'" . $key . "'";
+    //echo "$key";
 
-        if(isset($_GET['delete'])){ //if we pressed delete
+    if(isset($_GET['delete'])){ //if we pressed delete
 
-        $sql = "DELETE FROM country WHERE Country_abb=($inssql)";
+    $sql = "DELETE FROM country WHERE Country_abb=($delsql)";
 
-        if(mysqli_query($conn,$sql)){
-            echo "<p> <font color=white>Row deleted";
-        }
-        else
-            echo "<p> <font color=white>Row not deleted";
+    if(mysqli_query($conn,$sql)){
+        echo "<p> <font color=white>Deleted the entry for $name";
+    }
+    else{
+        echo "<p> <font color=white>$name not deleted<br>";
+        echo "Error: " .mysqli_error($conn);
+    }
 
-        echo "<br>";
-        echo "<br>";
+    echo "<br>";
+
+    // print code
+    echo "<br>$sql<br>";
+
+    echo "<br>";
     echo "<table border='1' cellpadding='2' bordercolor='#f0932b' width = '100%'>";
     echo "<tr>";
-    
+
         if(!empty($_GET["col2"])){
             foreach($_GET["col2"] as $col2){
                 echo"<th><p> <font color=white>$col2</th>";
@@ -40,8 +47,8 @@
                     foreach($_GET["col2"] as $col2){
                         echo "<input type='hidden' value='$col2' name='col2[]'>";
                     }
-                echo'<input type="hidden" name="primKey" value='.$row["Country_abb"].'> <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete">';
-                echo'<input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>'; 
+                echo'<input type="hidden" name="primKey" value='.$row["Country_abb"].'> <input type="hidden" name="name" value='.$row["Country_Name"].'> <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete">';
+                echo'<input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>';
                 echo "</tr>";
             }
         }
@@ -53,7 +60,7 @@
 
         echo "<p> <font color=white>Enter information:";
         echo "<br>";
-       
+
         echo "<form action = 'update2.php' method = 'get'>";
 
         if(!empty($_GET["col2"])){
@@ -66,6 +73,7 @@
                 echo "<br>";
             }
             echo "<input type = 'submit' name = 'update' value = 'UPDATE' id='update1_btn' class='update1'></form>";
+            echo "<br>";
         }
 
         echo "<br>";
@@ -92,7 +100,7 @@
                 foreach($_GET["col2"] as $col2){
                     echo "<input type='hidden' value='$col2' name='col2[]'>";
                 }
-                echo'<input type="hidden" name="primKey" value='.$row["Country_abb"].'> <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete"> <input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>';
+                echo'<input type="hidden" name="primKey" value='.$row["Country_abb"].'> <input type="hidden" name="name" value='.$row["Country_Name"].'> <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete"> <input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>';
                 echo "</tr>";
             }
         }
