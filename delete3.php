53719d15cb73217a39c17f1aabe83cebeadd65b4
joewong00 <63445582+joewong00@users.noreply.github.com>
2020-04-30 12:45:34 +0800

    Update delete3.php

--- delete3.php
+++ delete3.php
@@ -2,26 +2,32 @@
     include_once 'demos/demo1.php';
     include 'cwindex.php';
 
-        $key = $_GET["primKey"];
-        $delsql1 = "'" . $key[0] . "'";
-        $delsql2 = "'" . $key[1] . "'";
-        //echo "$key";
+    $key = $_GET["primKey"];
+    $delsql1 = "'" . $key[0] . "'";
+    $delsql2 = "'" . $key[1] . "'";
+    //echo "$key";1 1fg69                                                                                                                                                          
 
-        if(isset($_GET['delete'])){ //if we pressed delete
+    if(isset($_GET['delete'])){ //if we pressed delete
 
-        $sql = "DELETE FROM lang WHERE Country_abb=($delsql1) AND Language=($delsql2) ";
+    $sql = "DELETE FROM lang WHERE Country_abb=($delsql1) AND Language=($delsql2) ";
 
-        if(mysqli_query($conn,$sql)){
-            echo "<p> <font color=white>Row deleted";
-        }
-        else
-            echo "<p> <font color=white>Row not deleted";
+    if(mysqli_query($conn,$sql)){
+        echo "<p> <font color=white>Deleted the entry for $key[1] for $key[0]";
+    }
+    else{
+        echo "<p> <font color=white>Row not deleted<br>";
+        echo "Error: " .mysqli_error($conn);
+    }
 
-        echo "<br>";
-        echo "<br>";
+    echo "<br>";
+
+    // print code
+    echo "<br>$sql<br>";
+
+    echo "<br>";
     echo "<table border='1' cellpadding='2' bordercolor='#fff200' width = '100%'>";
     echo "<tr>";
-    
+
         if(!empty($_GET["col3"])){
             foreach($_GET["col3"] as $col3){
                 echo"<th><p> <font color=white>$col3</th>";
@@ -43,7 +49,7 @@
                     }
                 echo'<input type="hidden" name="primKey[]" value='.$row["Country_abb"].'> <input type="hidden" name="primKey[]" value='.$row["Language"].'>
                 <input type="submit" name="delete" value="DELETE" id="delete_btn" class= "delete">';
-                echo'<input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>'; 
+                echo'<input type="submit" name="update" value="UPDATE" id="update_btn" class= "update"></form></td>';
                 echo "</tr>";
             }
         }
@@ -55,7 +61,7 @@
 
         echo "<p> <font color=white> Enter information:";
         echo "<br>";
-       
+
         echo "<form action = 'update3.php' method = 'get'>";
 
         if(!empty($_GET["col3"])){
@@ -64,12 +70,13 @@
                     echo "$col3<input type = 'hidden', value = '$key[0]', name = 'primKey[]' checked> : <input type = 'text' value = '$key[0]' id='datacol' class='data'>";
                 else if($col3 == 'Language')
                     echo "$col3<input type = 'hidden', value = '$key[1]', name = 'primKey[]' checked> : <input type = 'text' value = '$key[1]' id='datacol' class='data'>";
-                else 
+                else
                     echo "$col3<input type = 'hidden', value = '$col3', name = 'col3[]' checked> : <input type = 'text' name = 'updcol3[]' id='datacol' class='data'>";
                 echo "<br>";
                 echo "<br>";
             }
             echo "<input type = 'submit' name = 'update' value = 'UPDATE' id='update1_btn' class='update1'></form>";
+            echo "<br>";
         }
 
         echo "<br>";
